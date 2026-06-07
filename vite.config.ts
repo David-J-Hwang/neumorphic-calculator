993e5be3c7d67b2eeabef00cb846946db51cfca5
David J. Hwang <jeongjaehwang@gmail.com>
2026-06-07 16:12:33 +0900

    Update vite.config.ts file for GitHub Pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  base: '/neumorphic-calculator/',
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
